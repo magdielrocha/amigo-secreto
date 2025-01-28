a34643caa5ac53eac1f622e6ad927deb53ab28a8
Magdiel Rocha <magdiel002@gmail.com>
2025-01-28 18:51:41 -0300

    adiciona funcionalidades e atualização do README

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+//adiciona os nomes no array
+function adicionarAmigo() {
+    let input = document.getElementById('amigo');
+    let nomes = input.value.trim();
+
+    if (nomes === '') {
+        alert('Digite um nome válido');
+        input.focus();
+    } else {
+        amigos.push(nomes);
+        limpaCampo();
+        atualizarLista();
+    }
+}
+//Limpa o campo do input
+function limpaCampo() { 
+    let campo = document.querySelector('input');
+    campo.value = '';
+}
+
+//Atualiza a lista de nomes
+function atualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for (let amigo of amigos) {
+        let li = document.createElement('li');
+        li.textContent = amigo; //Define o texto do <li> com o nome do amigo
+        lista.appendChild(li); //Adiciona o <li> à lista
+        //console.log(lista);
+    }
+
+}
+//Sorteia um nome da lista
+function sortearAmigo() {
+    let resultado = document.getElementById('resultado');
+    if(amigos.length === 0) {
+        alert('Adicione pelo menos um nome antes de realizar o sorteio.')
+        return;
+    }
+    let indiceSorteado = Math.floor(Math.random() * amigos.length);
+    let nomeSorteado = amigos[indiceSorteado];
+    resultado.innerHTML = `${nomeSorteado}`;
+    
+
+}
+
+
